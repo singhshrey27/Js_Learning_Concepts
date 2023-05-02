40ac3f27477ef16b42b849a5425e5ffd9a07cd28
shiransh <sp999pratap@gmail.com>
2023-05-03 03:01:41 +0530

    day2

--- Js_Day2/Dom.js
+++ Js_Day2/Dom.js
@@ -1,1 +1,4 @@
 //Dom - document object model
+
+//Dom show that how our overall structure looks
+//It is also usefull in manipulating a particular section by selecting it
---
+++ Js_Day2/grabElement.js
@@ -0,0 +1,10 @@
+//Two ways of selecting the elements on web pages:
+
+//1.querySelector
+//2.getelementby
+
+var title=document.getElementsByName("h1");
+[0].innerText
+var result=document.getElementsByClassName("container");
+
+//document.queryselector("classname");
