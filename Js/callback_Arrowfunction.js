f99e2d6d3a4701ddf6029b9ff87a39d92740d0f9
shiransh <sp999pratap@gmail.com>
2023-05-02 18:43:20 +0530

    file updated

--- Js/callback_Arrowfunction.js
+++ Js/callback_Arrowfunction.js
@@ -1,4 +1,5 @@
 var isEven=(element)=>{
+    //instead of writing the function we use the arrow 
     //arrow fucntions requires a return type
     // if(element%2==0){
     //     return true;
@@ -11,12 +12,23 @@ var isEven=(element)=>{
 
 console.log(isEven(4));
 
-var result=[2,3,6,8].every(isEven);
-//it will work for every element in the array
+
+
+var result=[2,4,6,8].every(isEven);
+//it will work for every element in the array on isEven
 console.log(result);
 //giving output false as here one elment is odd
 
 
-//Callbacks:
-var result=[2,3,6,8].every((e)=>(e%2===0));
+
+//Callbacks: This is the arrow callback
+//Method-1:
+var result=[2,3,6,8].every((e)=>{
+    return e % 2 ===0;
+});
 console.log(result);
+
+//Method-2:
+var result=[2,3,6,8].every((e)=>(e%2 === 0));
+console.log(result);
+//in case of these braces we don't have to use the return type
