8e9d8a6f36809a533601dba436db7ccf45e0af23
shiransh <sp999pratap@gmail.com>
2023-05-03 01:56:01 +0530

    project added

---
+++ Project1/script.js
@@ -0,0 +1,34 @@
+//set Timeout and setinterval
+
+//setTimout(function,milliseconds)
+//for a period of time it get's time out
+
+//setInterval(funciton,milliseconds)
+//for the interval of time it is working
+
+var counter=document.querySelector(".counter");
+var followers=document.querySelector(".followers");
+
+counter.innerHTML=2000;
+//innerHTML is used to access the inner thing inside the class
+
+let count=1;
+setInterval(()=>{
+    if(count<1000)
+      count++;
+      counter.innerHTML=count; 
+},1)
+//time is given of 1 millisecond that in every one milliseconds the content is getting changed here
+
+//setTimeout:
+
+setTimeout(()=>{
+    followers.innerHTML="1000 subscribers reached";
+
+},7000)
+
+//here time is 7k milliseconds 
+
+
+
+//change after 1000miliseconds
